057fcd66b9cf9e8dd58a678880a7b2f90aece65d
Yousef-Asal <yousefasal50@gmail.com>
2024-12-16 17:30:03 +0200

    .

--- app/agmad_family_farm/main.py
+++ app/agmad_family_farm/main.py
@@ -91,6 +91,7 @@ def read_light():
 
     time.sleep(0.1)
 
+
     # Read data back from 0x0C(12) with command register, 0x80(128), 2 bytes
     # ch0 LSB, ch0 MSB
     data = bus.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
