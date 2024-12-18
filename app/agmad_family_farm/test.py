b920a78d6e4a1055c45c972b9b32d71e48e0ddc3
Yousef-Asal <yousefasal50@gmail.com>
2024-12-18 03:13:23 +0200

    .

--- app/agmad_family_farm/test.py
+++ app/agmad_family_farm/test.py
@@ -352,7 +352,7 @@ def read_light():
     print("*****************")
     try:
         while True:
-            if GPIO.input(ch0) > 800:
+            if ch0 > 800:
                 print("Water level HIGH: Increasing fan speed & forward direction")
                 send_command("FAN_FORWARD")  # Set fan direction to forward
                 send_command("FAN_SPEED 200")  # Set fan speed (e.g., 200/255)
